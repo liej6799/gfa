62552ddf12694e8a18e64fdee0383f48927a200a
liej6799 <52598803+liej6799@users.noreply.github.com>
2021-09-11 15:20:52 +0800

    fix date filtering issue

--- web/angular/src/app/purchase/purchase.component.ts
+++ web/angular/src/app/purchase/purchase.component.ts
@@ -33,8 +33,8 @@ export class PurchaseComponent implements OnInit {
   constructor(public readonly list: ListService, private purchaseService: PurchaseService, private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.form.controls.startDate.setValue(formatDate(new Date(),'yyyy-MM-dd','en'))
-    this.form.controls.endDate.setValue(formatDate(new Date().setMonth(new Date().getMonth() - 1),'yyyy-MM-dd','en'))
+    this.form.controls.startDate.setValue(formatDate(new Date().setMonth(new Date().getMonth() - 1),'yyyy-MM-dd','en'))
+    this.form.controls.endDate.setValue(formatDate(new Date(),'yyyy-MM-dd','en'))
     this.form.controls.groupBy.setValue(PurchaseGroup.None)
 
     this.onChanges();
