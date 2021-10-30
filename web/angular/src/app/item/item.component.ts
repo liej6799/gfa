56b63c3057bfed5fb2c35e3c4a52908ebab537f5
liej6799 <52598803+liej6799@users.noreply.github.com>
2021-10-30 16:04:12 +0800

    fix pagination, item with new table implemented.

--- web/angular/src/app/item/item.component.ts
+++ web/angular/src/app/item/item.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ItemService, ItemDto } from '@proxy/items';
 import { PurchaseItemService, PurchaseItemDto } from '@proxy/purchases';
 import { Output, EventEmitter } from '@angular/core';
+import { SortEvent } from '../helper/NgbdSortableHeader';
 
 @Component({
   selector: 'app-item',
@@ -23,10 +24,18 @@ export class ItemComponent implements OnInit {
   selectedItemQuantityTracker = '';
   item = { items: [], totalCount: 0 } as PagedResultDto<ItemDto>;
 
+  page = 1;
+  pageSize = 10;
+
+  column: string = '';
+  direction: string = '';
+
   constructor(public readonly list: ListService, private itemService: ItemService) { }
 
   ngOnInit() {
-    const itemStreamCreator = (query) => this.itemService.getListFilter(query);
+    const itemStreamCreator = (query) => this.itemService.getListFilter({...query, 
+      skipCount: (this.page - 1) * this.pageSize,
+      sorting: this.column + ' ' + this.direction });
 
     this.list.hookToQuery(itemStreamCreator).subscribe((response) => {
       this.item = response;
@@ -54,6 +63,13 @@ export class ItemComponent implements OnInit {
 
   viewQuantityTracker(id: string) {
     this.selectedItemQuantityTracker = id;
+  }
+
+  onSort({ column, direction }: SortEvent) {
+    console.log(column);
+    this.column = column;
+    this.direction = direction;
+    this.ngOnInit();
   }
 }
 
