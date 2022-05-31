5ab8aa1af3a4217c900cd7c8de492a2fd2741e3c
liej6799 <52598803+liej6799@users.noreply.github.com>
2022-05-31 21:53:34 +0800

    fix date issue

--- mobile/app/src/main/java/com/example/gfamobile/data/model/Date.java
+++ mobile/app/src/main/java/com/example/gfamobile/data/model/Date.java
@@ -1,6 +1,7 @@
 package com.example.gfamobile.data.model;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 import java.util.Calendar;
 
@@ -9,8 +10,8 @@ public class Date {
     private DateTime endCalendar;
 
     public Date() {
-        this.startCalendar = new DateTime();
-        this.endCalendar = new DateTime();
+        this.startCalendar = new DateTime(DateTimeZone.UTC);
+        this.endCalendar = new DateTime(DateTimeZone.UTC);
     }
 
     public DateTime getStartCalendar() {
@@ -18,7 +19,7 @@ public class Date {
     }
 
     public void setStartCalendar(int year, int month, int day) {
-        this.startCalendar = new DateTime(year, month, day, 0, 0);
+        this.startCalendar = new DateTime(year, month, day, 0, 0, 0, 0,  DateTimeZone.UTC);
     }
 
     public DateTime getEndCalendar() {
@@ -26,6 +27,6 @@ public class Date {
     }
 
     public void setEndCalendar(int year, int month, int day) {
-        this.endCalendar = new DateTime(year, month, day, 0, 0);
+        this.endCalendar = new DateTime(year, month, day, 0, 0, 0, 0, DateTimeZone.UTC);
     }
 }
